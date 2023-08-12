7894fb4bc1e7256aa975e62644cf773cda71a6c7
Gabriel Aviles <gabrielaviles240@gmail.com>
2023-08-12 10:59:40 -0600

    Terminar

--- src/medical-records/entities/medical-record.entity.ts
+++ src/medical-records/entities/medical-record.entity.ts
@@ -1,1 +1,11 @@
-export class MedicalRecord {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { Patient } from 'src/users/entities/patient.entity';
+
+@Schema()
+export class MedicalRecord extends Document {
+  date: Date;
+  patient: Patient | Types.ObjectId;
+}
+
+export const MedicalRecordSchema = SchemaFactory.createForClass(MedicalRecord);
